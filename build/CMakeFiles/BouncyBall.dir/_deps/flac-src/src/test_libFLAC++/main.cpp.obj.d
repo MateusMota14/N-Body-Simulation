d4ab0959aba6bd904a700c9f7ec452b2f4a1f4b8
MateusMota14 <mota.luis@ime.eb.br>
2025-12-30 01:46:51 -0300

    ignore build folder

--- build/CMakeFiles/BouncyBall.dir/_deps/flac-src/src/test_libFLAC++/main.cpp.obj.d
+++
@@ -1,5 +0,0 @@
-CMakeFiles/BouncyBall.dir/_deps/flac-src/src/test_libFLAC++/main.cpp.obj: \
- C:\Users\Mateus\ Mota\Documents\Estudo\Projetos\BouncyBall\build\_deps\flac-src\src\test_libFLAC++\main.cpp \
- C:\Users\Mateus\ Mota\Documents\Estudo\Projetos\BouncyBall\build\_deps\flac-src\src\test_libFLAC++\decoders.h \
- C:\Users\Mateus\ Mota\Documents\Estudo\Projetos\BouncyBall\build\_deps\flac-src\src\test_libFLAC++\encoders.h \
- C:\Users\Mateus\ Mota\Documents\Estudo\Projetos\BouncyBall\build\_deps\flac-src\src\test_libFLAC++\metadata.h
--- build/CMakeFiles/BouncyBall.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for BouncyBall.
-# This may be replaced when dependencies are built.
